9e26670333c801827e29083b0c78537d7926dc27
Vijay Bairagi <vijayvaishnav906@gmail.com>
2025-07-24 16:00:50 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+
+AWS_S2_BUCKET_NAME = 'wafer-fault'
+MONGO_DATABASE_NAME = 'pwskills'
+MONGO_COLLECTION_NAME  = 'waferfault'
+
+TARGET_COLUMN = 'quality'
+MONGO_DB_URL = "mongodbtsrv://snshrivas:Snshrivas@clustere. u4&c&.mongodb.net/?retryMrites=true&w-majority"
+
+MODEL_FULL_NAME = 'model'
+MODEL_FILE_EXTENSION = '.pkl'
